94336c3923fc89044709ab178b026eeddb716c10
Wqst32 <tomasz09kos@gmail.com>
2026-03-12 23:43:34 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import cv2
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "AI parking działa"
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    file = request.files["image"]
+    file.save("image.jpg")
+
+    # tutaj AI analizuje zdjęcie
+
+    return "ok"
+
+app.run(host="0.0.0.0", port=8080)
