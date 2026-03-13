2aab6bbb70d4994243091ece3fdaeb70f1770519
Wqst32 <tomasz09kos@gmail.com>
2026-03-13 17:04:45 +0100

    Refactor database connection in update route
    Refactored database connection and updated route handling.

--- server.py
+++ server.py
@@ -3,41 +3,20 @@ import mysql.connector
 
 app = Flask(__name__)
 
-def connect_db():
-    return mysql.connector.connect(
-        host="mysql://root:mlUVabLxUQjJwkoBuADmrmtpaJcVEQMD@mysql.railway.internal:3306/railway",
-        user="root",
-        password="mlUVabLxUQjJwkoBuADmrmtpaJcVEQMD",
-        database="parking"
+@app.route("/update")
+def update():
+
+    db = mysql.connector.connect(
+        host="HOST",
+        user="USER",
+        password="PASSWORD",
+        database="DATABASE"
     )
 
-@app.route("/update")
-def update_db():
-
-    db = connect_db()
     cursor = db.cursor()
-
     cursor.execute("UPDATE parking SET status='test' WHERE id=1")
     db.commit()
 
-    return "baza zmieniona"
-
-print("SERVER START")
-import mysql.connector
-
-print("SERVER START")
-
-db = mysql.connector.connect(
-        host="mysql://root:mlUVabLxUQjJwkoBuADmrmtpaJcVEQMD@mysql.railway.internal:3306/railway",
-        user="root",
-        password="mlUVabLxUQjJwkoBuADmrmtpaJcVEQMD",
-        database="parking"
-)
-
-cursor = db.cursor()
-cursor.execute("UPDATE parking SET status='test' WHERE id=1")
-db.commit()
-
-print("ZMIENIONO BAZE")
+    return "OK"
 
 app.run(host="0.0.0.0", port=8080)
